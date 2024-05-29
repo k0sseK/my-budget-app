26bc3b06fdf12500fb7de78cf36e7a44835646b4
k0sseK <k0ssekcs@gmail.com>
2024-05-30 01:01:08 +0200

    add: login and register form component

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <!-- <div>1</div>
+  <div>2</div> -->
+    <RouterView />
+</template>
+
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<style scoped></style>
