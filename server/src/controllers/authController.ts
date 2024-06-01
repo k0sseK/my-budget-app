2adbb63a69a1341aee7a292d36b9edcd99e4bd99
k0sseK <k0ssekcs@gmail.com>
2024-06-01 21:08:00 +0200

    update(server/controllers): auth controller

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,40 +1,40 @@
-import { Request, Response } from 'express';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import { createUser, findUserByUsername, User } from '../models/userModel';
-import { secretKey } from '../config';
+import { Request, Response } from 'express'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import { createUser, findUserByUsername, User } from '../models/userModel'
+import { secretKey } from '../config'
 
 export const register = async (req: Request, res: Response) => {
-    const { username, email, password } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const { username, email, password } = req.body
+    const hashedPassword = await bcrypt.hash(password, 10)
 
     const newUser: User = {
         username,
         email,
         password: hashedPassword
-    };
+    }
 
     try {
-        await createUser(newUser);
-        res.status(201).json({ message: 'User registered successfully' });
+        await createUser(newUser)
+        res.status(201).json({ message: 'User registered successfully' })
     } catch (error) {
-        res.status(500).json({ message: 'Error registering user', error });
+        res.status(500).json({ message: 'Error registering user', error })
     }
-};
+}
 
 export const login = async (req: Request, res: Response) => {
-    const { username, password } = req.body;
-    const user = await findUserByUsername(username);
+    const { username, password } = req.body
 
+    const user = await findUserByUsername(username)
     if (!user) {
-        return res.status(400).json({ message: 'Invalid username or password' });
+        return res.status(400).json({ message: 'Invalid username' })
     }
 
-    const isMatch = await bcrypt.compare(password, user.password);
+    const isMatch = await bcrypt.compare(password, user.password)
     if (!isMatch) {
-        return res.status(400).json({ message: 'Invalid username or password' });
+        return res.status(400).json({ message: 'Invalid password' })
     }
 
-    const token = jwt.sign({ id: user.id, username: user.username }, secretKey, { expiresIn: '1h' });
-    res.json({ token });
-};
+    const token = jwt.sign({ id: user.id, username: user.username }, secretKey, { expiresIn: '1h' })
+    res.json({ token })
+}
