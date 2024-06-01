0529e3b6dc68327048a76754424f13e7446c0dba
k0sseK <k0ssekcs@gmail.com>
2024-06-01 21:06:58 +0200

    add(server): cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,18 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import cors from 'cors'
 import authRoutes from './routes/authRoutes'
 
 const app = express()
 const PORT = process.env.PORT || 3000
+
+app.use(
+    cors({
+        origin: 'http://localhost:5173',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        credentials: true
+    })
+)
 
 app.use(bodyParser.json())
 
