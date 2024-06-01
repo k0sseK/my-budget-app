e6bdbbdac21d1ddc80395d995bfcd5d99fa00b31
k0sseK <k0ssekcs@gmail.com>
2024-06-01 21:07:42 +0200

    feat(services): register, login auth

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -2,17 +2,28 @@ import axios from 'axios'
 
 const API_URL = 'http://localhost:3000/api/auth'
 
-export const register = (username: string, email: string, password: string) => {
-    return axios.post(`${API_URL}/register`, {
-        username,
-        email,
-        password
-    })
+export const register = async (username: string, email: string, password: string) => {
+    try {
+        const response = await axios.post(`${API_URL}/register`, {
+            username,
+            email,
+            password
+        })
+        return response.data
+    } catch (error) {
+        throw error
+    }
 }
 
-export const login = (username: string, password: string) => {
-    return axios.post(`${API_URL}/login`, {
-        username,
-        password
-    })
+export const login = async (username: string, password: string) => {
+    try {
+        const response = await axios.post(`${API_URL}/login`, {
+            username,
+            password
+        })
+
+        return response.data
+    } catch (error) {
+        throw error
+    }
 }
