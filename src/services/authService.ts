3387f1692d6fdebf5dbdbbd78b0ca2f87a87a2de
k0sseK <k0ssekcs@gmail.com>
2024-06-03 22:15:11 +0200

    refactor: Remove commented out code and update authentication middleware

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -29,17 +29,6 @@ export const login = async (username: string, password: string) => {
     }
 }
 
-export const authenticateToken = async () => {
-    try {
-        const token = localStorage.getItem('token')
-        const response = await axios.get(`${API_URL}/authenticateToken`, {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        })
-
-        return response.data
-    } catch (error) {
-        throw error
-    }
+export const logout = () => {
+    localStorage.removeItem('token')
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,1 +1,50 @@
-<template>sd</template>
+<template>
+    <div class="absolute top-0 left-0 w-full">
+        <div class="navbar bg-base-100">
+            <div class="navbar-start">
+                <a class="btn btn-ghost text-xl">Budget Manager</a>
+            </div>
+            <div class="navbar-center hidden lg:flex">
+                <ul class="menu menu-horizontal px-1 space-x-1">
+                    <li class="text-accent bg-neutral rounded-lg"><a>Summary</a></li>
+                    <li><a>Transactions</a></li>
+                    <li><a>Wallets</a></li>
+                </ul>
+            </div>
+            <div class="navbar-end">
+                <div class="dropdown dropdown-end">
+                    <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+                        <div class="w-9 rounded-full">
+                            <img
+                                alt="profile-picture"
+                                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                            />
+                        </div>
+                    </div>
+                    <ul
+                        tabindex="0"
+                        class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+                    >
+                        <li>
+                            <a class="justify-between"> Profile </a>
+                        </li>
+                        <li><a>Settings</a></li>
+                        <li @click="logoutUser()"><a>Logout</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+import { logout } from '../services/authService'
+
+const router = useRouter()
+
+const logoutUser = () => {
+    logout()
+    router.push('/')
+}
+</script>
