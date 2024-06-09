bb09e3b13a6eebb798cfbbb4c16de4970b28b5f4
k0sseK <k0ssekcs@gmail.com>
2024-06-09 23:09:34 +0200

    refactor: Improve WelcomeView layout and animations

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -1,27 +1,32 @@
 <template>
-    <div class="max-w-screen-xl flex items-center justify-center">
-        <div class="flex flex-row items-center gap-6">
-            <div class="flex items-center justify-center w-2/4">
-                <img class="w-4/5" src="@/assets/img/logo.png" alt="logo" />
-            </div>
-            <div class="flex flex-col w-2/4 self-start">
-                <div class="text-8xl font-bold" style="color: #f2f2f2">Manage your budget</div>
-                <div class="text-6xl font-semibold mt-20" style="color: rgba(235, 235, 235, 0.815)">
-                    Join us today.
+    <div class="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]">
+        <div class="w-[80rem]">
+            <div class="flex flex-row items-center gap-6">
+                <div data-aos="fade-left" class="flex items-center justify-center w-2/4">
+                    <img class="w-4/5" src="@/assets/img/logo.png" alt="logo" />
                 </div>
-                <div class="flex flex-col mt-5 gap-2">
-                    <button
-                        class="btn btn-md btn-wide btn-outline btn-accent w-[70%]"
-                        @click="handleForm('login')"
+                <div data-aos="fade-right" class="flex flex-col w-2/4 self-start">
+                    <div class="text-8xl font-bold" style="color: #f2f2f2">Manage your budget</div>
+                    <div
+                        class="text-6xl font-semibold mt-20"
+                        style="color: rgba(235, 235, 235, 0.815)"
                     >
-                        Login
-                    </button>
-                    <button
-                        class="btn btn-md btn-wide btn-neutral w-[70%]"
-                        @click="handleForm('register')"
-                    >
-                        Register
-                    </button>
+                        Join us today.
+                    </div>
+                    <div class="flex flex-col mt-5 gap-2">
+                        <button
+                            class="btn btn-md btn-wide btn-outline btn-accent w-[70%]"
+                            @click="handleForm('login')"
+                        >
+                            Login
+                        </button>
+                        <button
+                            class="btn btn-md btn-wide btn-neutral w-[70%]"
+                            @click="handleForm('register')"
+                        >
+                            Register
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
