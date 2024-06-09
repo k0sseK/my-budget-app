01130c336aeb8831c740d6f429060bbd9f7392db
k0sseK <k0ssekcs@gmail.com>
2024-06-09 23:08:58 +0200

    refactor: Add TabTitle component for displaying tab titles and descriptions

---
+++ src/components/TabTitle.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1 data-aos="fade-up" class="text-2xl font-bold text-slate-200 mb-1">{{ title }}</h1>
+        <p data-aos="fade-up" class="text-sm font-medium w-3/6">{{ description }}</p>
+    </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    title?: string
+    description?: string
+}>()
+</script>
